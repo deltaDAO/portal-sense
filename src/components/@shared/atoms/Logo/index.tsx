9afcb6fbcfd966e5eabfe7c7330aea810c1836ab
Kevin Rohlf <123676073+KevinRohlf@users.noreply.github.com>
2025-09-14 17:34:35 +0200

    feat: update header

--- src/components/@shared/atoms/Logo/index.tsx
+++ src/components/@shared/atoms/Logo/index.tsx
@@ -1,13 +1,11 @@
 import { ReactElement } from 'react'
-import LogoAsset from '@images/pontusx_logo_horizontal.svg'
-import LogoAssetSmall from '@images/pontusx_logo.svg'
+import LogoAsset from '@images/sense-blue.svg'
 import styles from './index.module.css'
 
 export default function Logo(): ReactElement {
   return (
     <div className={styles.logoWrapper}>
       <LogoAsset className={styles.logo} />
-      <LogoAssetSmall className={styles.logoSmall} />
     </div>
   )
 }
--- src/components/Header/SearchBar.tsx
+++ src/components/Header/SearchBar.tsx
@@ -61,9 +61,7 @@ export default function SearchBar({
     }
   }, [isSearchBarVisible, homeSearchBarFocus])
 
-  async function startSearch(
-    e: FormEvent<HTMLButtonElement | HTMLInputElement>
-  ) {
+  async function startSearch(e: FormEvent<HTMLFormElement | HTMLInputElement>) {
     e.preventDefault()
 
     if (value === '') setValue(' ')
@@ -88,7 +86,7 @@ export default function SearchBar({
     }
   }
 
-  async function handleButtonClick(e: FormEvent<HTMLButtonElement>) {
+  async function handleButtonClick(e: FormEvent<HTMLFormElement>) {
     e.preventDefault()
     await startSearch(e)
   }
@@ -101,8 +99,19 @@ export default function SearchBar({
     config: { mass: 1, tension: 140, friction: 12 }
   })
 
+  const showSearchBar = isSearchBarVisible || isSearchPage
+
+  if (!showSearchBar) return null
+
   return (
-    <form className={styles.search} autoComplete={!value ? 'off' : 'on'}>
+    <form
+      className={styles.search}
+      autoComplete={!value ? 'off' : 'on'}
+      onSubmit={(e) => {
+        e.preventDefault()
+        handleButtonClick(e)
+      }}
+    >
       <animated.div style={springStile} className={styles.springContainer}>
         <InputElement
           ref={searchBarRef}
@@ -116,7 +125,7 @@ export default function SearchBar({
           className={styles.input}
           onKeyPress={handleKeyPress}
         />
-        <button onClick={handleButtonClick} className={styles.button}>
+        <button type="submit" className={styles.button}>
           <SearchIcon className={styles.searchIcon} />
         </button>
       </animated.div>
