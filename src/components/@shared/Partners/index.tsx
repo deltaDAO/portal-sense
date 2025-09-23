4438a6b422ed901dbf61a9ac3cf48806b5240e14
Kevin Rohlf <123676073+KevinRohlf@users.noreply.github.com>
2025-09-23 11:44:12 +0200

    refactor: remove unused extended prop and extra logos logic from Partners component

--- src/components/@shared/Partners/index.tsx
+++ src/components/@shared/Partners/index.tsx
@@ -5,19 +5,12 @@ import classNames from 'classnames/bind'
 const cx = classNames.bind(styles)
 
 export default function Partners({
-  extended,
   className
 }: {
-  extended?: boolean
   className?: string
 }): ReactElement {
   const partners = require
     .context('../../../../public/images/partners', false, /\.(png|jpe?g)$/)
-    .keys()
-    .filter((e) => e.startsWith('./'))
-    .map((x) => x.replace('./', ''))
-  const extra = require
-    .context('../../../../public/images/extra-logos', false, /\.(png|jpe?g)$/)
     .keys()
     .filter((e) => e.startsWith('./'))
     .map((x) => x.replace('./', ''))
@@ -38,14 +31,6 @@ export default function Partners({
             src={`/images/partners/${logo}`}
           />
         ))}
-        {extended &&
-          extra?.map((logo) => (
-            <img
-              key={logo}
-              className={styles.logo}
-              src={`/images/extra-logos/${logo}`}
-            />
-          ))}
       </div>
     </>
   )
